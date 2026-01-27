2eb6c282506ee97cd8c0958ab39c60fee303f4ff
Arjun Tandon <113589883+truelearnerarjun@users.noreply.github.com>
2026-01-27 17:16:40 +0000

    added some changes

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -10,8 +10,8 @@ yum install -y httpd
 # Simple web page
 cat <<EOF >/var/www/html/index.html
 <h1>Server Details</h1>
-<p><strong>Hostname:</strong> $(hostname)</p>
-<p><strong>IP Address:</strong> $(hostname -I | awk '{print $1}')</p>
+<p><strong>Hostname is :</strong> $(hostname)</p>
+<p><strong>IP Address is :</strong> $(hostname -I | awk '{print $1}')</p>
 EOF
 
 # Enable + start Apache
